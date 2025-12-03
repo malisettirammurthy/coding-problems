146ed41409ebd45f26dfa8786f175ba538b8a1d2
Ram Murthy Malisetti <malisettirammurthy@gmail.com>
2025-12-04 01:26:42 +0530

    Golang concurrency - buffered channel - use select and handle chan closed case. With timeout example as-well

--- golang/concurrency/main.go
+++ golang/concurrency/main.go
@@ -18,9 +18,11 @@ type Order struct {
 func main() {
 	// var wg sync.WaitGroup
 	wg := sync.WaitGroup{}
-	wg.Add(2)
+	wg.Add(3)
 
 	orderChan := make(chan *Order, 20)
+	processedChan := make(chan *Order, 20)
+
 	go func() {
 		defer wg.Done()
 		for _, order := range generateOrders(20) {
@@ -30,23 +32,56 @@ func main() {
 		fmt.Println("Done with generating orders")
 	}()
 
-	go processOrders(orderChan, &wg)
+	go processOrders(orderChan, processedChan, &wg)
+
+	go func() {
+		defer wg.Done()
+		for {
+			select {
+			// Read from processedChan
+			case processedOrder, ok := <-processedChan:
+				// Handle when processedChan is closed:
+				if !ok {
+					fmt.Println(
+						"Processing channel closed",
+					)
+					return
+				}
+				fmt.Printf(
+					"Processed order %d with status: %s\n",
+					processedOrder.ID,
+					processedOrder.Status,
+				)
+				// Timeout
+				// case <-time.After(300 * time.Millisecond):
+				// 	fmt.Println(
+				// 		"Timeout waiting for operations.",
+				// 	)
+				// 	return
+			}
+		}
+	}()
 
 	wg.Wait()
 	fmt.Println("All operations completed. Exiting.")
 }
 
 func processOrders(
-	orderChan <-chan *Order,
+	inChan <-chan *Order,
+	outChan chan<- *Order,
 	wg *sync.WaitGroup,
 ) {
-	defer wg.Done()
-	for order := range orderChan {
+	defer func() {
+		wg.Done()
+		close(outChan)
+	}()
+	for order := range inChan {
 		time.Sleep(
 			time.Duration(rand.Intn(500)) *
 				time.Millisecond,
 		)
-		fmt.Printf("Processing Order %d\n", order.ID)
+		order.Status = "Processed"
+		outChan <- order
 	}
 }
 
