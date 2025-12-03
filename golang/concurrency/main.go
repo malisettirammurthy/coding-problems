f3cb849bc5064967d4b89819c9a3bc1db95d5750
Ram Murthy Malisetti <malisettirammurthy@gmail.com>
2025-12-04 00:31:17 +0530

    Golang Concurrency example

--- golang/concurrency/main.go
+++ golang/concurrency/main.go
@@ -12,7 +12,13 @@ import (
 type Order struct {
 	ID     int
 	Status string
+	mu     sync.Mutex
 }
+
+var (
+	totalUpdates int
+	updateMutex  sync.Mutex
+)
 
 func main() {
 	// var wg sync.WaitGroup
@@ -21,40 +27,49 @@ func main() {
 
 	orders := generateOrders(20)
 
-	go func() {
-		defer wg.Done()
-		processOrders(orders)
-	}()
+	// go func() {
+	// 	defer wg.Done()
+	// 	processOrders(orders)
+	// }()
 
-	go func() {
-		defer wg.Done()
-		updateOrderStatuses(orders)
-	}()
-
-	go func() {
-		defer wg.Done()
-		reportOrderStatus(orders)
-	}()
+	for i := 0; i < 3; i++ {
+		go func() {
+			defer wg.Done()
+			for _, order := range orders {
+				updateOrderStatus(order)
+			}
+		}()
+	}
 
 	wg.Wait()
+	reportOrderStatus(orders)
 	fmt.Println("All operations completed. Exiting.")
+	fmt.Printf("Total Updates: %d\n", totalUpdates)
 }
 
-func updateOrderStatuses(orders []*Order) {
-	for _, order := range orders {
-		time.Sleep(
-			time.Duration(rand.Intn(300)) *
-				time.Millisecond,
-		)
-		status := []string{
-			"Processing", "Shipped", "Delivered",
-		}[rand.Intn(3)]
-		order.Status = status
-		fmt.Printf(
-			"Updated order %d status: %s\n",
-			order.ID, status,
-		)
-	}
+func updateOrderStatus(order *Order) {
+	order.mu.Lock()
+	time.Sleep(
+		time.Duration(rand.Intn(300)) *
+			time.Millisecond,
+	)
+	status := []string{
+		"Processing", "Shipped", "Delivered",
+	}[rand.Intn(3)]
+	order.Status = status
+	fmt.Printf(
+		"Updated order %d status: %s\n",
+		order.ID, status,
+	)
+	order.mu.Unlock()
+
+	updateMutex.Lock()
+	defer updateMutex.Unlock()
+
+	currentUpdates := totalUpdates
+	time.Sleep(5 * time.Millisecond)
+	totalUpdates = currentUpdates + 1
+
 }
 
 func processOrders(orders []*Order) {
@@ -76,15 +91,12 @@ func generateOrders(count int) []*Order {
 }
 
 func reportOrderStatus(orders []*Order) {
-	for i := 0; i < 5; i++ {
-		time.Sleep(1 * time.Second)
-		fmt.Println("\n--- Order Status Report ---")
-		for _, order := range orders {
-			fmt.Printf(
-				"Order %d: %s\n",
-				order.ID, order.Status,
-			)
-		}
-		fmt.Println("---------------------")
+	fmt.Println("\n--- Order Status Report ---")
+	for _, order := range orders {
+		fmt.Printf(
+			"Order %d: %s\n",
+			order.ID, order.Status,
+		)
 	}
+	fmt.Println("---------------------")
 }
