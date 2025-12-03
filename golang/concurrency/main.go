be2156e765f75ed666851b4cb7675e8d8db6473a
Ram Murthy Malisetti <malisettirammurthy@gmail.com>
2025-12-04 00:46:47 +0530

    Golang concurrency - Generate and process using a channel

--- golang/concurrency/main.go
+++ golang/concurrency/main.go
@@ -15,65 +15,33 @@ type Order struct {
 	mu     sync.Mutex
 }
 
-var (
-	totalUpdates int
-	updateMutex  sync.Mutex
-)
-
 func main() {
 	// var wg sync.WaitGroup
 	wg := sync.WaitGroup{}
-	wg.Add(3)
+	wg.Add(2)
 
-	orders := generateOrders(20)
+	orderChan := make(chan *Order)
+	go func() {
+		defer wg.Done()
+		for _, order := range generateOrders(20) {
+			orderChan <- order
+		}
+		close(orderChan)
+		fmt.Println("Done with generating orders")
+	}()
 
-	// go func() {
-	// 	defer wg.Done()
-	// 	processOrders(orders)
-	// }()
-
-	for i := 0; i < 3; i++ {
-		go func() {
-			defer wg.Done()
-			for _, order := range orders {
-				updateOrderStatus(order)
-			}
-		}()
-	}
+	go processOrders(orderChan, &wg)
 
 	wg.Wait()
-	reportOrderStatus(orders)
 	fmt.Println("All operations completed. Exiting.")
-	fmt.Printf("Total Updates: %d\n", totalUpdates)
 }
 
-func updateOrderStatus(order *Order) {
-	order.mu.Lock()
-	time.Sleep(
-		time.Duration(rand.Intn(300)) *
-			time.Millisecond,
-	)
-	status := []string{
-		"Processing", "Shipped", "Delivered",
-	}[rand.Intn(3)]
-	order.Status = status
-	fmt.Printf(
-		"Updated order %d status: %s\n",
-		order.ID, status,
-	)
-	order.mu.Unlock()
-
-	updateMutex.Lock()
-	defer updateMutex.Unlock()
-
-	currentUpdates := totalUpdates
-	time.Sleep(5 * time.Millisecond)
-	totalUpdates = currentUpdates + 1
-
-}
-
-func processOrders(orders []*Order) {
-	for _, order := range orders {
+func processOrders(
+	orderChan <-chan *Order,
+	wg *sync.WaitGroup,
+) {
+	defer wg.Done()
+	for order := range orderChan {
 		time.Sleep(
 			time.Duration(rand.Intn(500)) *
 				time.Millisecond,
